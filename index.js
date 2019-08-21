f0f88d96c580fe181ed48be728f3716e5a901b5d
Wellmtb <wellington.serafim@outlook.com>
2019-08-21 01:43:09 -0300

    Feature: Crud de usuários

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 
 //query params = ?teste=1
 //Route Params = /user/1
 //Request body = { "name": "Well", "email": "well@email.com"}
 
 const usuarios = ["Gustavo", "Well", "Renan", "Keicy"];
+
+server.get("/users", (req, res) => {
+  return res.json(usuarios);
+});
+
 server.get("/user/:index", (req, res) => {
   const { index } = req.params;
 
   console.log("Requisição recebida");
   return res.json(usuarios[index]);
+});
+
+server.post("/user", (req, res) => {
+  const { name } = req.body;
+
+  usuarios.push(name);
+  return res.json(usuarios);
+});
+
+server.put("/user/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  usuarios[index] = name;
+
+  return res.json(usuarios);
+});
+
+server.delete("/user/:index", (req, res) => {
+  const { index } = req.params;
+
+  usuarios.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
